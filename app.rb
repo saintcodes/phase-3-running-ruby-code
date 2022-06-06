9b960e1e355c8de5761f9d860691f5a85dfa40fc
Michelle <clairsin@gmail.com>
2022-06-06 10:25:58 -0400

    commit tree 4d3b26058ffc18b2b23d50432e9255b5f690dd36

---
+++ app.rb
@@ -0,0 +1,22 @@
+# # console.logs("Hello world!")
+puts "Hello World!"
+puts "Hello world!"
+
+
+puts [1,2,3].inspect
+p [1,2,3].inspect
+pp [1,2,3].inspect
+print [1,2,3].inspect
+
+print "Pass this test, please."
+print "Hello world!"
+print "Hello world!"  
+
+p "Hello world with one p!"  
+p "Hello world with one p!"  
+p "Hello world with one p!"  
+
+pp "Hello world!"  
+pp "Hello world!"  
+pp "Hello world!"  
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
